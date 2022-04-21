7579f0e50166e8f82d52028472984624ec138476
DucQuang <lynguyencong2081999@gmail.com>
2022-04-22 00:55:06 +0700

    home_above

---
+++ index.js
@@ -0,0 +1,42 @@
+function getById(id) {
+  return document.getElementById(id);
+}
+function getByClass(c) {
+  return document.getElementsByClassName(c);
+}
+function expandHeader() {
+  var container = getById("container_id");
+  var navbar = getById("navbar_id");
+  var cancel = getById("cancel_");
+  var expand = getById("expand_");
+  var button = getById("nav_button");
+  var nav = getByClass("nav_");
+
+  for (var i = 0; i < nav.length; i++) {
+    nav[i].classList.add("show_margin");
+  }
+  container.classList.add("show_container");
+  navbar.classList.add("show_navbar");
+  button.classList.add("show_margin");
+  cancel.classList.add("high_z");
+}
+function cancelHeader() {
+  var container = getById("container_id");
+  var cancel = getById("cancel_");
+  var expand = getById("expand_");
+
+  container.classList.remove("show_container");
+  cancel.classList.remove("high_z");
+}
+function toggle_dropdown() {
+  getById("drop_menu").classList.toggle("show_dropdown");
+}
+
+function countUp() {
+  var i = 0;
+  var element = getById("number_1");
+  for (var i = 0; i <= peakNum; i++) {
+    element.innerText = i;
+    setTimeout(function(){},1000);
+  }
+}
