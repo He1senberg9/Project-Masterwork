f565df0bfae4aa215b3bd35251137f226a93c284
Nguyen Duc Thien <ducthiennguyen1601@gmail.com>
2022-05-06 00:19:26 +0700

    add toggle.js

---
+++ toggle.js
@@ -0,0 +1,23 @@
+function toggle(){
+    
+    
+    var business_ = document.querySelector(".business_")
+    business_.classList.toggle("dark")
+    var showcase_ = document.querySelector(".showcase_")
+    showcase_.classList.toggle("dark")
+    var solutions = document.querySelector(".solutions")
+    solutions.classList.toggle("dark")
+    var general = document.querySelector(".general")
+    general.classList.toggle("dark")
+    var completed = document.querySelector(".completed")
+    completed.classList.toggle("dark")
+    var community = document.querySelector(".community")
+    community.classList.toggle("dark")
+    var footer = document.querySelector(".footer")
+    footer.classList.toggle("dark")
+    // var business_ = document.querySelector("business_")
+    // body.classList.toggle("dark")
+    // var business_ = document.querySelector("business_")
+    // body.classList.toggle("dark")
+
+}
